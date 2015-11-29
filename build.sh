11cc05fd1104ec81884e662682148fc8f9c64c63
John Hogenmiller <john@yourtech.us>
2015-11-29 14:23:35 -0500

    Bump to 0.5.2

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 TIMESTAMP=`date +%s`
 TYPES="rpm deb"
+VERSION="0.5.2"
+BASE_URL="https://dl.bintray.com/mitchellh/consul"
 
 # Download sources
 if [ ! -f rootfs/usr/bin/consul ]
 then
   mkdir -p rootfs/usr/bin
   echo "Downloading sources (1/2)..."
-  URL=https://dl.bintray.com/mitchellh/consul/0.5.0_linux_amd64.zip
+  URL=${BASE_URL}/${VERSION}_linux_amd64.zip
   wget -qO tmp.zip $URL && unzip tmp.zip -d rootfs/usr/bin && rm tmp.zip
   chmod 0755 rootfs/usr/bin/consul
 fi
@@ -16,7 +18,7 @@ if [ ! -d rootfs/usr/share/consul-ui ]
 then
   mkdir -p rootfs/usr/share
   echo "Downloading sources (2/2)..."
-  URL=https://dl.bintray.com/mitchellh/consul/0.5.0_web_ui.zip
+  URL=URL=${BASE_URL}/${VERSION}_web_ui.zip
   wget -qO tmp.zip $URL && unzip tmp.zip -d rootfs/usr/share && rm tmp.zip
   mv rootfs/usr/share/dist rootfs/usr/share/consul-ui
   rm rootfs/usr/share/consul-ui/.gitkeep
